83a4faded49733db80a7f0ff8c3639066079df58
Ryo-Hatakeyama <19940901ryo.hatakeyama@gmail.com>
2024-03-30 22:49:49 +0900

    コミット

---
+++ server/server.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+
+const app = express();
+const PORT = 5000;
+// app.get("/", (req, res) => res.send("Hello"));
+
+const t = initTRPC.create();
+const router = t.router;
+const publicProcedure = t.procedure;
+
+interface Todo {
+  id: string;
+  content: string;
+}
+
+const todoList: Todo[] = [
+  {
+    id: "1",
+    content: "散歩",
+  },
+  {
+    id: "2",
+    content: "プログラミング",
+  },
+];
+
+// publicProcedure.query();
+// publicProcedure.
+
+const appRouter = router({
+  test: publicProcedure.query(() => {
+    return "TEST TRPC";
+  }),
+  getTodos: publicProcedure.query(() => {
+    return todoList;
+  }),
+});
+
+app.use("/trpc", trpcExpress.createExpressMiddleware({ router: appRouter }));
+
+app.listen(PORT);
